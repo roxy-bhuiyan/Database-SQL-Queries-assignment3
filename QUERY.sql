8c551a8361c0de8c2ef4e4b86b8f31361770f9a2
roxy-bhuiyan <majadul.roxybhuiyan@gmail.com>
2026-06-15 01:45:07 +0600

    create user table

--- QUERY.sql
+++ QUERY.sql
@@ -9,3 +9,17 @@ DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
 
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE Users (
+    user_id      SERIAL,
+    full_name    VARCHAR(100)        NOT NULL,
+    email        VARCHAR(150)        NOT NULL,
+    role         VARCHAR(50)         NOT NULL,
+    phone_number VARCHAR(20),
+ 
+    CONSTRAINT pk_users PRIMARY KEY (user_id),
+    CONSTRAINT uq_users_email UNIQUE (email),
+    CONSTRAINT chk_users_role CHECK (role IN ('Ticket Manager', 'Football Fan'))
+);
