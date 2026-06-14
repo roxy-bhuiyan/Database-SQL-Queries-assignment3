b54e895f841b00a49005deb460402e2775c4fa5f
roxy-bhuiyan <majadul.roxybhuiyan@gmail.com>
2026-06-15 00:06:30 +0600

    create first

---
+++ QUERY.sql
@@ -0,0 +1,11 @@
+-- =========================================================================
+-- SYSTEM: Football Ticket Booking System Database Setup
+-- DESCRIPTION: Full DDL + DML + Queries Solution
+-- =========================================================================
+
+
+-- DROP TABLES IF THEY ALREADY EXIST TO PREVENT CONFLICTS
+DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS Matches;
+DROP TABLE IF EXISTS Users;
+
